94ff303f5672966cab17e69fadd8374d89d71073
LazyDoomSlayer <lazydoomslayer@gmail.com>
2025-06-08 16:35:23 +0200

    refactor: Move rust analyzer to diff file.

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,24 @@
+return {
+  "neovim/nvim-lspconfig",
+
+  opts = {
+    servers = {
+
+      rust_analyzer = {
+        settings = {
+          ["rust-analyzer"] = {
+            checkOnSave = {
+              command = "clippy",
+            },
+            cargo = {
+              autoReload = true,
+            },
+            formatting = {
+              provider = "rustfmt",
+            },
+          },
+        },
+      }
+    },
+  },
+}
